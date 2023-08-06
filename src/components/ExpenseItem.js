b98f2a2f68c193cbd19e76b605297458fba47c16
Ershad-Alam-67 <ealam6732@gmail.com>
2023-08-06 17:46:14 +0530

    new ExpenseDetails component added(location added)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,11 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
     </div>
   )
 }
